bb4f57ca0ceaf300b25e3cff4fffa7e2870d0e76
Zachary Owen <dev@scrptr.com>
2012-04-21 17:06:34 -0400

    added application routing, responding json barebones..going

--- co.artavenue.service/app.js
+++ co.artavenue.service/app.js
@@ -1,1 +1,15 @@
-//
+var handle, req_handler, router, server;
+
+  server = require("./server");
+  router = require("./router");
+  req_handler = require("./req_handler");
+
+  handle = {};
+  handle["/"] = req_handler.start;
+  handle["/start"] = req_handler.start;
+  handle["/upload"] = req_handler.upload;
+  handle["/db"] = req_handler.db;
+  handle["/fb"] = req_handler.fb;
+
+  server.start(router.route, handle);
+
