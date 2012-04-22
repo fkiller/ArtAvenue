6e3e51033a981d4d2df28742662d686faccc6e0b
Zachary Owen <dev@scrptr.com>
2012-04-22 14:15:24 -0400

    push

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  
+  var order = ['0','3','4','5','1','2','6','7','8','9'];
+  var sel = 0;
+
+  setInterval(function() {
+    //$('#demospace').append("img goes here<br />");
+    
+    $('img:first-child').animate({
+      'css': 'sss',
+      'css': 'sss'
+    });
+
+
+
+    
+  }, 10000);
+
+});
